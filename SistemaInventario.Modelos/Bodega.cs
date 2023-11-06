305cd97cde68ed1df6c12c4da604f78f51a81670
jasx22 <98923303+jasx22@users.noreply.github.com>
2023-11-05 20:14:10 -0600

    Configuracion del proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarioV6.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre es requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre solo admite un max de 60 caracteres")]
+        public String Nombre { get; set; }
+
+        [Required(ErrorMessage = "Descripción es requerido")]
+        [MaxLength(100, ErrorMessage = "Descripción solo admite un max de 100 caracteres")]
+        public String Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
